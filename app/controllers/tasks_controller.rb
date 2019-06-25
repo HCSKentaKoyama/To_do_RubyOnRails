ba8ac526b6e4366551a9a7de1fefdbc37198b199
vagrant <vagrant@localhost.localdomain>
2019-06-25 01:11:04 +0000

    add
    tasks リスト化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+    before_action :authenticate_user, {only: [:index]}
+    # before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+
     def index
-        
+        @tasks = Task.where(user_id: @current_user.id)
     end
 end
